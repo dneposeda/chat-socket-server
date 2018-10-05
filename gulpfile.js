c4ef43890596d4e8c6fe569e74c254b259305f95
Denis <den.neposeda@gmail.com>
2018-10-05 23:31:35 +0400

    edit gulpfile + typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 const gulp = require('gulp');
+// const gp = require('gulp-load-plugins')();
 const ts = require('gulp-typescript');
-
-// pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
-//task to be run when the watcher detects changes
+
+var SRC_DIR = './src/';
+var DIST_DIR = './dist/';
+var path = {
+	typescript: {
+		// entry: SRC_DIR + 'typescript/index.ts',
+		src: SRC_DIR + 'typescript/**/*.ts',
+		dist: DIST_DIR + 'js'
+	}
+};
+
 gulp.task('scripts', () => {
-  const tsResult = tsProject.src()
-  .pipe(tsProject());
-  return tsResult.js.pipe(gulp.dest(''));
-});
-//set up a watcher to watch over changes
-
-gulp.task('watch', ['scripts'], () => {
-  	gulp.watch('**/*.ts', ['scripts']);	
+  return tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest(path.typescript.dist));
 });
 
-gulp.task('default', ['watch']);
+gulp.task('watch', () => {
+	gulp.watch(path.typescript.src, gulp.series('scripts'));
+});
+
+gulp.task('default', gulp.series(
+  gulp.parallel('scripts'),
+  gulp.parallel('watch')
+));
